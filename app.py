76f202f4ed8092cf1607daeeeda59d27a8bd9e9b
anujml <131592463+anujml@users.noreply.github.com>
2023-11-18 12:54:01 +0000

    finish_project

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template
+import datetime
+import os
 import pymongo
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, url_for
 
-mongodb_url = 'mongodb+srv://Brock1234:Brock1234@cluster0.y5ykvso.mongodb.net/'
-myclient = pymongo.MongoClient(mongodb_url)
+load_dotenv()
 
 app = Flask(__name__)
 
+# mongodb_url = 'mongodb+srv://Brock1234:Brock1234@cluster0.y5ykvso.mongodb.net/microblog'
+client = pymongo.MongoClient(os.getenv("MONGODB_URI"))
+app.db = client.microblog
 
-@app.route('/')
+entries = []
+
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-  return render_template('home.html', mongodb_url=mongodb_url)
+  if request.method == "POST":
+    entry_content = request.form.get('content')
+    formatted_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    app.db.entries.insert_one({
+        'content': entry_content,
+        'date': formatted_date
+    })
+
+  entries_with_date = [
+      (entry['content'], entry['date'],
+       datetime.datetime.strptime(entry['date'], '%Y-%m-%d').strftime('%b %d'))
+      for entry in app.db.entries.find({})
+  ]
+  return render_template('home.html', entries=entries_with_date)
 
 
 if __name__ == '__main__':
